295a2d28e60bef9e26c5a16c8b3afd27666fcba0
Vera152022 <118746256+Vera152022@users.noreply.github.com>
2025-06-02 11:11:49 +0300

    serial

--- MT/Serializer.cs
+++ MT/Serializer.cs
@@ -109,7 +109,7 @@ namespace MT
             var text = File.ReadAllText(FilePath);
             var top = JsonConvert.DeserializeObject<Top>(text);
 
-             
+            if (top == null) return new int[0]; 
 
             int[] top_top = top.Top_10;
             return top_top;
